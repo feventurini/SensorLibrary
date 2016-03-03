d12366ee5bfea9ad603ba31a94652939eefec24a
Federico Baldassarre <baldassarre.fe@gmail.com>
2016-03-03 18:50:58 +0100

    Clean up del codice di Rfid_SL030, abbastanza confuso

--- src/implementations/Rfid_SL030.java
+++ src/implementations/Rfid_SL030.java
@@ -34,29 +34,16 @@ import sensor.SensorParameter;
 import sensor.SensorState.State;
 
 public class Rfid_SL030 extends SensorServer implements RfidSensor {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 4894977624049261879L;
-
-	// Raspberry Pi's I2C bus
-	private static final int i2cBus = 1;
 
 	// RFID addresses
 	private static final int DEVICE_ADDRESS = 0x50;
-
-	// Read RFID command
-	private static final byte getRfidCmd = (byte) 0x01;
-	private static final byte firmwareRfidCmd = (byte) 0xF0;
-
 	// I2C bus
-	I2CBus bus;
+	private I2CBus bus;
+	private GpioPinDigitalInput trigger;
 
 	@SensorParameter(userDescription = "Trigger pin", propertyName = "trigger")
 	public Integer triggerPinNumber;
-
-	private GpioPinDigitalInput trigger;
 
 	private ConcurrentLinkedQueue<FutureResultImpl<String>> queue;
 
@@ -73,32 +60,20 @@ public class Rfid_SL030 extends SensorServer implements RfidSensor {
 
 		if (!allParametersFilledUp()) {
 			state.setState(State.SETUP);
-		}
-
-		else {
+		} else {
 			final GpioController gpio = GpioFactory.getInstance();
-
-			System.out.println(triggerPinNumber);
-			Pin triggerPin = RaspiPin.getPinByName("GPIO " + triggerPinNumber);
-			System.out.println(triggerPin);
-			// provision gpio pin #02 as an input pin with its internal pull
+			// provision gpio pin as an input pin with its internal pull
 			// down
 			// resistor enabled
+			Pin triggerPin = RaspiPin.getPinByName("GPIO " + triggerPinNumber);
 			trigger = gpio.provisionDigitalInputPin(triggerPin, PinPullResistance.PULL_DOWN);
 			trigger.setDebounce(1000);
-
-			try {
-				bus = I2CFactory.getInstance(I2CBus.BUS_1);
-				System.out.println("Connected to bus OK!!!");
-
-				// get device itself
-				sl030 = bus.getDevice(DEVICE_ADDRESS);
-
-				System.out.println("Connected to device OK!!!");
-				// Small delay before starting
-			} catch (IOException e) {
-				System.out.println("Exception: " + e.getMessage());
-			}
+			// get the bus
+			bus = I2CFactory.getInstance(I2CBus.BUS_1);
+			System.out.println("Connected to bus");
+			// get device itself
+			sl030 = bus.getDevice(DEVICE_ADDRESS);
+			System.out.println("Connected to device");
 
 			trigger.addListener(new GpioPinListenerDigital() {
 				@Override
@@ -109,29 +84,26 @@ public class Rfid_SL030 extends SensorServer implements RfidSensor {
 						try {
 							rfid = readRfid();
 							System.out.println("Letto RFID " + rfid);
-							if (!rfid.equals("NO-TAG"))
-									while(!queue.isEmpty())
-										queue.poll().set(rfid);									
-								
+							if (!rfid.equalsIgnoreCase("NO-TAG"))
+								while (!queue.isEmpty())
+									queue.poll().set(rfid);
+
 						} catch (IOException e) {
 							e.printStackTrace();
 						}
 
 					}
 				}
-
 			});
-
 			state.setState(State.RUNNING);
 		}
 
 	}
-	
 
 	@Override
 	public void tearDown() {
-		// TODO Auto-generated method stub
-
+		state.setState(State.SHUTDOWN);
+		trigger.removeAllListeners();
 	}
 
 	public String readRfid() throws IOException {
@@ -148,26 +120,26 @@ public class Rfid_SL030 extends SensorServer implements RfidSensor {
 		}
 
 		byte[] writeBuffer = new byte[2];
-		String result = "NO-TAG";
 		try {
-			// SEND COMMAND TO DEVICE TO REQUEST FIRMWARE VERSION
+			// SEND COMMAND TO DEVICE TO REQUEST READING A MIFARE CARD
+			// | LENGTH | CMD |
 			writeBuffer[0] = (byte) 0x01; // LENGTH (CMD+DATA)
-			writeBuffer[1] = (byte) 0x01; // 0x01getRfidCmd; // COMMAND
-			sl030.write(writeBuffer, 0, 2);
+			writeBuffer[1] = (byte) 0x01; // CMD: SELECT MIFARE CARD
+			sl030.write(writeBuffer, 0, writeBuffer.length);
 
 			Thread.sleep(1000);
 
 			// READ SELECT-TAG RESPONSE
+			// | LENGTH | STATUS | UID (4-7 byte) | TYPE
 			// first just read in a single byte that represents the
-			// command+status+data payload
+			// command+status+data payload length
 			int length = sl030.read();
-
 			System.out.println("TOTAL BYTES AVAILABLE: " + length);
 
 			// if there are no remaining bytes (length == 0), then we can exit
 			// the function
 			if (length <= 0) {
-				System.out.format("Error: %n bytes read for LENGTH/n", length);
+				System.out.println("Error: " + length + " bytes read for LENGTH");
 				return "NO-TAG";
 			}
 
@@ -179,39 +151,29 @@ public class Rfid_SL030 extends SensorServer implements RfidSensor {
 			// validate to ensure we got back at least the command and status
 			// bytes
 			if (readTotal <= 3) {
-				System.out.format("Error: %n bytes read/n", readTotal);
+				System.out.println("Error: only " + readTotal + " bytes read");
 				return "NO-TAG";
 			}
 
-			byte leng = readBuffer[0];
+			byte len = readBuffer[0];
 			byte command = readBuffer[1]; // COMMAND BYTE
 			byte status = readBuffer[2]; // STATUS BYTES
 			byte type = readBuffer[readBuffer.length - 1];
 			byte[] uidByte = new byte[readBuffer.length - 4];
-			int k = 3;
 			for (int i = 0; i < uidByte.length; i++) {
-				uidByte[i] = readBuffer[i + k];
+				uidByte[i] = readBuffer[i + 3];
 			}
+
 			// now we need to get the payload data (if there is any?)
-			String uid = "?";
+			String uid = DatatypeConverter.printHexBinary(uidByte);
 
-			if (readTotal > 3) {
-				// uid=Base64.encodeBase64String(uidByte);
-				uid = DatatypeConverter.printHexBinary(uidByte);
-			}
-			result = uid;
-
-		} catch (IOException e) {
+			System.out.println("Tag read: " + uid);
+			return uid.trim();
+		} catch (IOException | InterruptedException e) {
 			System.out.println("Error: " + e.getMessage());
-		} catch (InterruptedException e) {
-			System.out.println("Interrupted Exception: " + e.getMessage());
+			state.setState(State.FAULT);
+			return "NO-TAG";
 		}
-		// System.out.println("Id RFID: " + result);
-		// if (!result.equalsIgnoreCase("NO-TAG"))
-		// {
-		// this.Display(result);
-		// }
-		return result;
 	}
 
 	@Override
@@ -222,7 +184,7 @@ public class Rfid_SL030 extends SensorServer implements RfidSensor {
 	@Override
 	public FutureResult<String> readTagAsync() throws RemoteException {
 		FutureResultImpl<String> result = new FutureResultImpl<>();
-		queue.add(result);			
+		queue.add(result);
 		return result;
 	}
 
